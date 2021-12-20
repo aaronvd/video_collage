88a87c7cdd005bc66a1df1192583af069044cc97
Aaron Diebold <aaron.diebold@duke.edu>
2021-12-20 16:52:42 -0500

    Made setup.py script.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='video_collage',
+      version='0.1',
+      description='Dynamically record and generate video from tiled sub-videos.',
+      url='https://github.com/aaronvd/video_collage',
+      author='Aaron V. Diebold',
+      author_email='diebolav@gmail.com',
+      license='MIT',
+      packages=['video_collage'],
+      zip_safe=False)
+
+
+
+
+
+
